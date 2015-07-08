0206a4965b05c454f5c48afb8521a5acfe87c8c5
ando <ando@Space.local>
2015-07-08 14:50:57 +0900

    No.2000-2002 Done.

--- app/src/main/java/net/cosmoway/furufuru_ball_android/GraphicsView.java
+++ app/src/main/java/net/cosmoway/furufuru_ball_android/GraphicsView.java
@@ -4,23 +4,70 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.view.View;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class GraphicsView extends View{
+public class GraphicsView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+    // 円のX,Y座標
+    private int mCircleX = 0;
+    private int mCircleY = 0;
+    // 円の移動量
+    private int mCircleVx = 5;
+    private int mCircleVy = 5;
+    // 描画用
+    private Paint mPaint;
 
+    //Constructor
     public GraphicsView(Context context) {
         super(context);
-        // Set the color of background.
-        setBackgroundColor(Color.BLUE);
+        // SurfaceView描画に用いるコールバックを登録する。
+        getHolder().addCallback(this);
+        // 描画用の準備
+        mPaint = new Paint();
+        mPaint.setColor(Color.RED);
+        // スレッド開始
+        Thread loop = new Thread(this);
+        loop.start();
     }
-    // Override the method as onDraw().
-    // Draw graphic use instance of class as canvas.
+
     @Override
-    protected void onDraw(Canvas canvas){
-        Paint paint = new Paint();
-        paint.setColor(Color.RED);
-        // Draw the circle.
-        paint.setAntiAlias(true);
-        canvas.drawCircle(120,200,100,paint);
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // SurfaceView生成時に呼び出されるメソッド。
+        // 今はとりあえず背景を青にするだけ。
+        Canvas canvas = holder.lockCanvas();
+        canvas.drawColor(Color.BLUE);
+        holder.unlockCanvasAndPost(canvas);
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void run() {
+        // Runnableインターフェースをimplementsしているので、runメソッドを実装する
+        // これは、Threadクラスのコンストラクタに渡すために用いる。
+        while (true) {
+            Canvas canvas = getHolder().lockCanvas();
+            if (canvas != null) {
+                canvas.drawColor(Color.BLUE);
+                // 円を描画する
+                canvas.drawCircle(mCircleX, mCircleY, 40, mPaint);
+                getHolder().unlockCanvasAndPost(canvas);
+                // 円の座標を移動させる
+                mCircleX += mCircleVx;
+                mCircleY += mCircleVy;
+                // 画面の領域を超えた？
+                if (mCircleX < 0 || getWidth() < mCircleX) mCircleVx *= -1;
+                if (mCircleY < 0 || getHeight() < mCircleY) mCircleVy *= -1;
+            }
+        }
     }
 }
--- app/src/main/java/net/cosmoway/furufuru_ball_android/MainActivity.java
+++ app/src/main/java/net/cosmoway/furufuru_ball_android/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new GraphicsView(this));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Thread.interrupted();
     }
 }
