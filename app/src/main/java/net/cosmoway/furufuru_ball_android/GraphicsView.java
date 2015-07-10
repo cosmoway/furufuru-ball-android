62b623df06cce3d075feb5723ba9613b9ab76031
ando <ando@Space.local>
2015-07-10 17:56:29 +0900

    Done.

--- app/src/main/java/net/cosmoway/furufuru_ball_android/GraphicsView.java
+++ app/src/main/java/net/cosmoway/furufuru_ball_android/GraphicsView.java
@@ -21,17 +21,26 @@ public class GraphicsView extends SurfaceView implements SurfaceHolder.Callback,
     private final int INIT_DIAMETER = 40;
     private int mDiameter = INIT_DIAMETER;
     // 円のX,Y座標
-    private int mCircleX = INIT_DIAMETER;
-    private int mCircleY = INIT_DIAMETER;
+    private float mCircleX = INIT_DIAMETER;
+    private float mCircleY = INIT_DIAMETER;
+    // Acceleraton
+    private float[] mAcceleration;
+    // 円の加速度
+    private float mCircleAx = 0.0f;
+    private float mCircleAy = 0.0f;
+    private float mCircleAz = 0.0f;
     // 円の移動量
-    private int mCircleVx = 5;
-    private int mCircleVy = 5;
+    private double mCircleVx = 0.0d;
+    private double mCircleVy = 0.0d;
     // 描画用
     private Paint mPaint;
     // Loop
     private Thread mLoop;
     // SensorManager
     private SensorManager mManager;
+
+    //
+    private final int SENSOR_DELAY;
     // Vibration
     private Vibrator mVib;
 
@@ -47,6 +56,9 @@ public class GraphicsView extends SurfaceView implements SurfaceHolder.Callback,
         mManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         mVib = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         mLoop = new Thread(this);
+        // Initializeing of acceleraton.
+        mAcceleration = new float[]{0.0f, 0.0f, 0.0f};
+        SENSOR_DELAY = SensorManager.SENSOR_DELAY_GAME;
     }
 
     @Override
@@ -66,16 +78,30 @@ public class GraphicsView extends SurfaceView implements SurfaceHolder.Callback,
         List<Sensor> sensors = mManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
         if (sensors.size() > 0) {
             Sensor s = sensors.get(0);
-            mManager.registerListener(this, s, SensorManager.SENSOR_DELAY_GAME);
+            mManager.registerListener(this, s, SENSOR_DELAY);
         }
         // スレッド開始
         mLoop.start();
     }
 
     @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        // Listenerの登録解除
-        mManager.unregisterListener(this);
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            mAcceleration = event.values.clone();
+            mCircleAx = -mAcceleration[0] / 100;
+            mCircleAy = mAcceleration[1] / 100;
+            mCircleAz = mAcceleration[2] / 100;
+            String str = "Acceleration:"
+                    + "\nX:" + mCircleAx * 100
+                    + "\nY:" + mCircleAy * 100
+                    + "\nZ:" + mCircleAz * 100;
+            Log.d("Acceleration", str);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
     @Override
@@ -90,34 +116,32 @@ public class GraphicsView extends SurfaceView implements SurfaceHolder.Callback,
                 canvas.drawCircle(mCircleX, mCircleY, mDiameter, mPaint);
                 getHolder().unlockCanvasAndPost(canvas);
                 // 円の座標を移動させる
-                mCircleX += mCircleVx;
-                mCircleY += mCircleVy;
+                mCircleVx += mCircleAx * SENSOR_DELAY;
+                mCircleVy += mCircleAy * SENSOR_DELAY;
+                mCircleX += mCircleVx * SENSOR_DELAY;
+                mCircleY += mCircleVy * SENSOR_DELAY;
                 // 画面の領域を超えた？
-                if (mCircleX < mDiameter || getWidth() < mCircleX + mDiameter) {
+                if (mCircleX < mDiameter || getWidth() - mDiameter < mCircleX) {
                     mVib.vibrate(50);
-                    mCircleVx *= -1;
+                    mCircleVx = -mCircleVx;
+                    mCircleAx = -mCircleAx;
+                    if (mCircleX < mDiameter) mCircleX = mDiameter;
+                    else mCircleX = getWidth() - mDiameter;
                 }
-                if (mCircleY < mDiameter || getHeight() < mCircleY + mDiameter) {
+                if (mCircleY < mDiameter || getHeight() - mDiameter < mCircleY) {
                     mVib.vibrate(50);
-                    mCircleVy *= -1;
+                    mCircleVy = -mCircleVy;
+                    mCircleAy = -mCircleAy;
+                    if (mCircleY < mDiameter) mCircleY = mDiameter;
+                    else mCircleY = getHeight() - mDiameter;
                 }
             }
         }
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
-        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            String str = "Values:"
-                    + "\nX:" + event.values[SensorManager.DATA_X]
-                    + "\nY:" + event.values[SensorManager.DATA_Y]
-                    + "\nZ:" + event.values[SensorManager.DATA_Z];
-            Log.d("Values",str);
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Listenerの登録解除
+        mManager.unregisterListener(this);
     }
 }
