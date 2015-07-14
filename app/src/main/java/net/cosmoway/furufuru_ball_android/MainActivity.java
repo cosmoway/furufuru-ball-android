77866fddb392fb39b00f4a06df27beaf3ee53e27
ando <ando@Space.local>
2015-07-14 14:22:03 +0900

    websocket

--- app/src/main/java/net/cosmoway/furufuru_ball_android/MainActivity.java
+++ app/src/main/java/net/cosmoway/furufuru_ball_android/MainActivity.java
@@ -5,16 +5,64 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class MainActivity extends Activity {
+
+    private final String TAG = "Ws";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new GraphicsView(this));
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if ("sdk".equals(Build.PRODUCT)) {
+            // エミュレータの場合はIPv6を無効    ----1
+            java.lang.System.setProperty("java.net.preferIPv6Addresses", "false");
+            java.lang.System.setProperty("java.net.preferIPv4Stack", "true");
+        }
+
+        try {
+            URI uri = new URI("ws://10.0.2.2:3333");
+            WebSocketClient client = new WebSocketClient(uri) {
+
+                @Override
+                public void onOpen(ServerHandshake handshake) {
+                    Log.d(TAG, "onOpen");
+                }
+
+                @Override
+                public void onMessage(final String message) {
+                    Log.d(TAG, "onMessage");
+                    Log.d(TAG, "Message:" + message);
+                }
+
+                @Override
+                public void onError(Exception ex) {
+                    Log.d(TAG, "onError");
+                    ex.printStackTrace();
+                }
+
+                @Override
+                public void onClose(int code, String reason, boolean remote) {
+                    Log.d(TAG, "onClose");
+                }
+            };
+
+            client.connect();
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
         Criteria criteria = new Criteria();
         criteria.setAccuracy(Criteria.ACCURACY_FINE);
         criteria.setBearingRequired(false);  // 方位不要
