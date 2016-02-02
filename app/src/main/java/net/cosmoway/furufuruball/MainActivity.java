d6a565db12c1a9632fde15ac2c7626d8cd6edf61
ANDO <ando@cosmoway.net>
2016-02-02 15:33:20 +0900

    パーミッション要求追加。

--- app/src/main/java/net/cosmoway/furufuruball/MainActivity.java
+++ app/src/main/java/net/cosmoway/furufuruball/MainActivity.java
@@ -9,6 +9,7 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
@@ -18,15 +19,14 @@ import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.ViewGroup.MarginLayoutParams;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
-
-import android.view.ViewGroup.MarginLayoutParams;
-import android.view.ViewGroup.LayoutParams;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -127,14 +127,14 @@ public class MainActivity extends Activity implements MyWebSocketClient.MyCallba
         criteria.setSpeedRequired(false);    // 速度不要
         criteria.setAltitudeRequired(false); // 高度不要
         if (locationManager != null) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            }
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission
+                    (this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
                 return;
             }
             locationManager.requestSingleUpdate(criteria, new LocationListener() {
@@ -259,7 +259,7 @@ public class MainActivity extends Activity implements MyWebSocketClient.MyCallba
                     result.setText("Time  ----");
                 } else {
                     gameSet.setVisibility(View.VISIBLE);
-                    result.setText("Time  " + ((double) mGraphicsView.mTime / 1000));
+                    result.setText(String.format("Time  %s", (double) mGraphicsView.mTime / 1000));
                 }
                 findViewById(R.id.button_back).setOnClickListener(new View.OnClickListener() {
                     @Override
